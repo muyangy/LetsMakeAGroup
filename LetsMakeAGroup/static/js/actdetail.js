929c89464c16e66ceaf172d474f159a20c89beff
Fangxiaoyu Feng <fengfangxiaoyu@gmail.com>
2013-12-05 03:54:53 -0500

    feedbackfollow

--- LetsMakeAGroup/static/js/actdetail.js
+++ LetsMakeAGroup/static/js/actdetail.js
@@ -12,7 +12,6 @@ $(window).load(function(){
       var userid = $('#useridhidden').text();
       var mark = $('#markhidden').text();
       var text = $('#markfollowerhidden').text();
-      alert("getin");
       if(actuserid!=userid && mark =="0" && !text){
         $.ajax({
           type: "GET",
