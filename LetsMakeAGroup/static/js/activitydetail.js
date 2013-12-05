637967dc69937a85b381f61b6415504d9e5d6868
Fangxiaoyu Feng <fengfangxiaoyu@gmail.com>
2013-12-05 17:31:52 -0500

    delete

--- LetsMakeAGroup/static/js/activitydetail.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('#joinpic')[0].click(function(){
-        var id = $('#useridhidden')[0].innerhtml;
-        $('#allfollowers')[0].append("<img src='/infophoto/"+id+"style='max-height:50px; max-width:50px;'>");
-
-    });
-});
--- Procfile
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-web: gunicorn FinalProject.wsgi
-=======
-web: gunicorn FinalProject.wsgi 
->>>>>>> 6fda1ba33ea13e548615b71623b4cc71c33b1e44
